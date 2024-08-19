f63f16178e588cb1133ae39769f0db47364952eb
Gabriel Pedro Veras <gabrielpedro.veras@gmail.com>
2024-08-19 12:06:25 -0300

    app-mobilidade

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,23 +2,15 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { ScheduleModule } from '@nestjs/schedule';
+import { HttpModule } from '@nestjs/axios';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { CacheModule } from '@nestjs/cache-manager';
 
 // Entidade de linhas
 import { Linha } from './Modules/Entity/Linha';
 import { LinhaService } from './Modules/Service/Linha';
 import { LinhaController } from './Modules/Controller/Linha';
 
-// Entidade de operadoras
-import { Operadora } from './Modules/Entity/Operadora';
-import { OperadoraService } from './Modules/Service/Operadora';
-import { OperadoraController } from './Modules/Controller/Operadora';
-
-// Entidade de Veiculos
-import { Veiculo } from './Modules/Entity/Veiculo';
-import { VeiculoService } from './Modules/Service/Veiculo';
-import { VeiculoController } from './Modules/Controller/Veiculo';
-
-// Entidade de Posicões
 import { Posicao } from './Modules/Entity/Posicao';
 import { PosicaoService } from './Modules/Service/Posicao';
 import { PosicaoController } from './Modules/Controller/Posicao';
@@ -28,12 +20,10 @@ import { Horaria } from './Modules/Entity/Horario';
 import { HorariaService } from './Modules/Service/Horaria';
 import { HorariaController } from './Modules/Controller/Horaria';
 
-// Entidade de itinerario
 import { Itinerario } from './Modules/Entity/Itinerario';
 import { ItinerarioService } from './Modules/Service/Itinerario';
 import { ItinerarioController } from './Modules/Controller/Itinerario';
 
-// Entidade de itinerario
 import { Parada } from './Modules/Entity/Parada';
 import { ParadaService } from './Modules/Service/Parada';
 import { ParadaController } from './Modules/Controller/Parada';
@@ -56,7 +46,5 @@ import { ParadaController } from './Modules/Controller/Parada';
     }),
     TypeOrmModule.forFeature([Linha, Operadora, Veiculo, Posicao, Horaria, Itinerario, Parada]),
   ],
-  providers: [LinhaService, OperadoraService, VeiculoService, PosicaoService, HorariaService, ItinerarioService, ParadaService],
-  controllers: [LinhaController, OperadoraController, VeiculoController, PosicaoController, HorariaController, ItinerarioController, ParadaController],
 })
 export class AppModule {}
